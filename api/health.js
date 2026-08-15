44e6747082815b7da1ac603c2bb6583009029155
okivictor01-bit <okivictor01@gmail.com>
2026-08-15 15:47:07 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,1 +1,7 @@
-
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    hasPaystackSecret: !!process.env.PAYSTACK_SECRET_KEY,
+    hasSupabaseService: !!process.env.SUPABASE_SERVICE_ROLE_KEY
+  });
+};
