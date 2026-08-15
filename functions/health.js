19f31bc597ead6570c76d3614fdf3ef98e7d19af
okivictor01-bit <okivictor01@gmail.com>
2026-08-15 15:44:58 +0100

    Update health.js

--- functions/health.js
+++ functions/health.js
@@ -1,1 +1,9 @@
-
+export async function onRequestGet(context) {
+  return new Response(JSON.stringify({
+    ok: true,
+    hasPaystackSecret: !!context.env.PAYSTACK_SECRET_KEY,
+    hasSupabaseService: !!context.env.SUPABASE_SERVICE_ROLE_KEY
+  }), {
+    headers: { 'content-type': 'application/json' }
+  });
+}
